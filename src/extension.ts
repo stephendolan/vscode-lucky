9e72c09e9da14aad7bc6aff9c096ed53f3ff3411
Stephen Dolan <dolan.stephen1@gmail.com>
2020-08-13 14:58:32 -0400

    Basic command set up with info messages

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,23 @@ import * as vscode from "vscode";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "lucky" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("lucky.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from Lucky! You did it!");
+  let editModel = vscode.commands.registerCommand("lucky.editModel", () => {
+    vscode.window.showInformationMessage("This would open the model file");
   });
+  context.subscriptions.push(editModel);
 
-  context.subscriptions.push(disposable);
+  let editOperation = vscode.commands.registerCommand("lucky.editOperation", () => {
+    vscode.window.showInformationMessage("This would open the operation file");
+  });
+  context.subscriptions.push(editOperation);
+
+  let editPage = vscode.commands.registerCommand("lucky.editPage", () => {
+    vscode.window.showInformationMessage("This would open the page file");
+  });
+  context.subscriptions.push(editPage);
+
+  let editAction = vscode.commands.registerCommand("lucky.editAction", () => {
+    vscode.window.showInformationMessage("This would open the action file");
+  });
+  context.subscriptions.push(editAction);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
